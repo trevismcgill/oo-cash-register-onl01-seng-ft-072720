d67f17159c0bcb2689ec8e62ab2c9c559e12de01
Trevis McGill <trevismcgill25@gmail.com>
2020-08-07 23:38:35 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,11 +19,13 @@ def apply_discount
   # binding.pry
   if @discount == 0
     "There is no discount to apply."
+  else
 discount_percent = @discount.to_f / 100
 new_total = (@total *= (1.0 - discount_percent)).to_i
 # new_total = new_total_float.to_i
 # binding.pry
 "After the discount, the total comes to $#{new_total}."
+end
 
 end
 
