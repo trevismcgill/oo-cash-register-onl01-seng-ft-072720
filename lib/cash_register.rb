0d91b20bf47e12e59190c569f5bc4e45814e8bfd
Trevis McGill <trevismcgill25@gmail.com>
2020-08-07 23:37:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,10 +18,11 @@ end
 def apply_discount
   # binding.pry
 discount_percent = @discount.to_f / 100
-new_total_float = (@total * (1.0 - discount_percent))
-new_total = new_total_float.to_i
+new_total = (@total *= (1.0 - discount_percent)).to_i
+# new_total = new_total_float.to_i
 # binding.pry
 "After the discount, the total comes to $#{new_total}."
+
 end
 
 def items(i1, i2)
