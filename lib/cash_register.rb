ec597c70b1d576ec91e21d823bd3134e4b80fbf7
Trevis McGill <trevismcgill25@gmail.com>
2020-08-07 22:44:04 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,4 +3,24 @@ class CashRegister
 def initialize
 @total = 0
 end
+
+def total
+
+end
+
+def add_item
+
+end
+
+def apply_discount
+
+end
+
+def items
+
+end
+
+def void_last_transaction
+
+end
 end
