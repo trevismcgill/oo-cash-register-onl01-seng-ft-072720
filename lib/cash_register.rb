d1d8b42de3fc2a34a38573a3b367e44aa8b43beb
Trevis McGill <trevismcgill25@gmail.com>
2020-08-07 23:30:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,7 +19,7 @@ def apply_discount
   # binding.pry
 discount_percent = @discount.to_f
 new_total = @total * 1.0 - discount_percent
-binding.pry
+# binding.pry
 puts "After the discount, the total comes to $#{new_total}"
 @total
 end
