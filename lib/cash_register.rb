aca52bfc314d240cd9ea7d001f8a4b35d38eb291
Trevis McGill <trevismcgill25@gmail.com>
2020-08-07 23:23:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,9 +16,9 @@ def add_item(title, price, quantity = 1)
 end
 
 def apply_discount
-  binding.pry
+  # binding.pry
 (@total * (100 - @discount.to_f))
-binding.pry
+# binding.pry
 puts "After the discount, the total comes to $#{@total}"
 end
 
